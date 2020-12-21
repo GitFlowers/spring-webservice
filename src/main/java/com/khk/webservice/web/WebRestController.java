67a9de1bdb32d859f550f8d5bdc36a70cee8cae2
GitFlowers <gusrn160304@gmail.com>
2020-12-21 18:43:26 +0900

    h2 db에 Posts 게시글 저장 API 추가

--- src/main/java/com/khk/webservice/web/WebRestController.java
+++ src/main/java/com/khk/webservice/web/WebRestController.java
@@ -1,13 +1,28 @@
 package com.khk.webservice.web;
 
+import com.khk.webservice.domain.PostsRepository;
+import com.khk.webservice.dto.posts.PostsSaveRequestDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebRestController {
 
+    private PostsRepository postsRepository;
+
+    public WebRestController(PostsRepository postsRepository) {
+        this.postsRepository = postsRepository;
+    }
+
     @GetMapping("/hello")
     public String hello() {
         return "HelloWorld";
+    }
+
+    @PostMapping("/posts")
+    public void savePosts(@RequestBody PostsSaveRequestDto dto) {
+        postsRepository.save(dto.toEntity());
     }
 }
